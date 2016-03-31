51a4967b0b3b6f8d15828efa232cc35e9fc94754
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2016-03-31 13:10:33 +0200

    Hide tabs in certain pages

--- venvastApp/www/app/app.js
+++ venvastApp/www/app/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic'])
 
-.run(function ($ionicPlatform) {
+.run(function ($ionicPlatform, $ionicTabsDelegate, $rootScope) {
     $ionicPlatform.ready(function () {
         if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -19,6 +19,14 @@ var app = angular.module('starter', ['ionic'])
         }
         if (window.StatusBar) {
             StatusBar.styleDefault();
+        }
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        if (toState.name == 'home.catagory') {
+            $ionicTabsDelegate.showBar(false);
+        } else {
+            $ionicTabsDelegate.showBar(true);
         }
     });
 })
